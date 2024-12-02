5befbbf78f37a0b93f7040ffb200de6dfb798cb3
hado0123 <hado_123@naver.com>
2024-12-02 16:28:55 +0900

    up

--- ch02/sol09.js
+++ ch02/sol09.js
@@ -3,6 +3,6 @@ const url = require('url')
 const website = 'https://www.example.com/path?query=123'
 
 // URL 파싱 후 호스트 이름 추출
-const parsedUrl = new url.URL(website)
+const parsedUrl = new URL(website)
 
 console.log(`Host: ${parsedUrl.host}`)
